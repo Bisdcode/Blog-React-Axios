8fd2fe30ca571f66bc4e8da26e9aee3a42379a3f
Bisdcode <bismarck.sdcode@gmail.com>
2025-01-19 12:57:54 -0300

    add getPosts p1

--- react-axios/src/routes/Home.jsx
+++ react-axios/src/routes/Home.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import { Link } from "react-router-dom";
+
+import axios from "axios";
+
+import "./Home.css";
 
 const Home = () => {
+	const [posts, setPosts] = useState([]);
+
+	// resgate dos dados da api
+	const getPosts = async () => {
+		try {
+			const response = await axios.get(
+				"https://jsonplaceholder.typicode.com/posts",
+			);
+
+			const data = response.data;
+
+			console.log(data);
+			// console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	// executando
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		getPosts();
+	}, []);
+
 	return <div>Home</div>;
 };
 
 export default Home;
-Home;
