95e8dffa3a4d5a1ce205ea0dbcabba701cd072ec
Bisdcode <bismarck.sdcode@gmail.com>
2025-01-19 12:12:45 -0300

    create navbar

--- react-axios/src/App.jsx
+++ react-axios/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 function App() {
 	return (
 		<>
 			<div className="App">
+				<Navbar />
 				<div className="container">
 					<Outlet />
-					<h1>React Axios</h1>
 				</div>
 			</div>
 		</>
