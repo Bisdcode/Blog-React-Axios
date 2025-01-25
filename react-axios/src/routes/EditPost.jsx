46622464011b06da2d6609c8fe5bf2655aa6d80a
Bisdcode <bismarck.sdcode@gmail.com>
2025-01-25 18:06:08 -0300

    edit post completed

--- react-axios/src/routes/EditPost.jsx
+++ react-axios/src/routes/EditPost.jsx
@@ -9,6 +9,36 @@ const EditPost = () => {
 	const [title, setTitle] = useState();
 	const [body, setBody] = useState();
 
+	const getPost = async () => {
+		try {
+			const response = await blogFetch.get(`/posts/${id}`);
+
+			const data = response.data;
+
+			setTitle(data.title);
+			setBody(data.body);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const editPost = async (e) => {
+		e.preventDefault();
+
+		const post = { title, body, userId: 1 };
+
+		try {
+			await blogFetch.put(`/posts/${id}`, { body: post });
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		getPost();
+	}, []);
+
 	return (
 		<div className="new-post">
 			<h2>Editando: {title}</h2>
@@ -21,6 +51,7 @@ const EditPost = () => {
 						id="title"
 						placeholder="Digite o título"
 						onChange={(e) => setTitle(e.target.value)}
+						value={title || ""}
 					/>
 				</div>
 				<div className="form-control">
@@ -30,6 +61,7 @@ const EditPost = () => {
 						id="body"
 						placeholder="Digite o conteúdo"
 						onChange={(e) => setBody(e.target.value)}
+						value={body || ""}
 					/>
 				</div>
 				<input type="submit" value="Editar Post" className="btn" />
