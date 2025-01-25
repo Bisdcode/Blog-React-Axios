8fc39e70d12cab3c36b9650acf84da8e7beaf761
Bisdcode <bismarck.sdcode@gmail.com>
2025-01-24 21:11:55 -0300

    post request NewPost

--- react-axios/README.md
+++ react-axios/README.md
@@ -1,7 +1,9 @@
 # React + Vite
 
 - Install:
+  "npm create vite@latest"
   "npm i axios react-router-dom"
+  "npm run dev"
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- react-axios/src/routes/NewPost.jsx
+++ react-axios/src/routes/NewPost.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
+import blogFetch from "../axios/config";
+import { useNavigate } from "react-router-dom";
+
 import "./NewPost.css";
 
 const NewPost = () => {
+	const navigate = useNavigate();
+
+	const [title, setTitle] = useState();
+	const [body, setBody] = useState();
+
+	const createPost = async (e) => {
+		e.preventDefault();
+
+		const post = { title, body, userId: 1 };
+
+		await blogFetch.post("/posts", {
+			body: post,
+		});
+
+		navigate("/");
+	};
+
 	return (
 		<div className="new-post">
 			<h2>Inserir novo Post:</h2>
-			<form>
+			<form onSubmit={(e) => createPost(e)}>
 				<div className="form-control">
 					<label htmlFor="title">Título:</label>
 					<input
@@ -12,11 +33,17 @@ const NewPost = () => {
 						name="title"
 						id="title"
 						placeholder="Digite o título"
+						onChange={(e) => setTitle(e.target.value)}
 					/>
 				</div>
 				<div className="form-control">
 					<label htmlFor="body">Conteúdo:</label>
-					<textarea name="body" id="body" placeholder="Digite o conteúdo" />
+					<textarea
+						name="body"
+						id="body"
+						placeholder="Digite o conteúdo"
+						onChange={(e) => setBody(e.target.value)}
+					/>
 				</div>
 				<input type="submit" value="Criar Post" className="btn" />
 			</form>
